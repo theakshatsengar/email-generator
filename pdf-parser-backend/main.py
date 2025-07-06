e5cbfdcd0639978ee3869b123e49ede0ef8dc4cb
Akshat <helloakshatsengar@gmail.com>
2025-07-06 13:31:18 +0530

    fixed port

--- pdf-parser-backend/main.py
+++ pdf-parser-backend/main.py
@@ -162,4 +162,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port) 
